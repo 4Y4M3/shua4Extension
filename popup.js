fc0d7eedda38f5ce336578f6899f588859280aee
AYAME <ayame1004@live.jp>
2021-12-22 19:07:52 +0900

    prototype

--- popup.js
+++ popup.js
@@ -1,45 +1,69 @@
-const form     = document.getElementById("Form");
-const target   = document.getElementById("URL");
-const delay    = document.getElementById("Delay");
-const progress = document.getElementById("Progress");
-const button   = document.getElementById("Run");
+import ExpressionParser from "./expression_parser.js";
+
+const DEFAULT_DELAY = 200;  // millisecond
+const DEBUG = false;
+
+const go = document.getElementById("Go");
+const form = document.getElementById("Form");
+const delay = document.getElementById("Delay");
+const target = document.getElementById("URL");
+const message = document.getElementById("Message");
 
 form.addEventListener("submit", handleFormSubmit);
+
+if (DEBUG) setMessage("init");
+delay.value = DEFAULT_DELAY;
+
+if(DEBUG) target.value = "https://ja.wikipedia.org/wiki/{{%d(1,5,2,3)}}"
 
 async function handleFormSubmit(event) {
     event.preventDefault();
 
     clearMessage();
+    if (DEBUG) setMessage("Start.");
+    go.innerHTML = "😍";
 
-    let url = stringToUrl(target.value);
-    if (!url) {
-        setMessage("Invalid URL");
-        return;
+    var urls = [];
+
+    const expressionParser = new ExpressionParser(target.value);
+    urls.push(
+        ...expressionParser.getUrls().map((url) => { return url; })
+    );
+
+    for (var url of urls) {
+        setMessage(url);
+        openUrl(url);
+        await sleep(getDelay());
     }
 
-    chrome.tabs.create({
-        url:url.toString(),
-        active:false
-      });
-
-    setMessage("OK");
+    if (DEBUG) setMessage("End.");
+    go.innerHTML = "😎";
 }
 
-function stringToUrl(input) {
-    // Start with treating the provided value as a URL
-    try {
-        return new URL(input);
-    } catch {}
-    // If that fails ¯\_(ツ)_/¯
-    return null;
+function openUrl(input) {
+    chrome.tabs.create({
+        url: input,
+        active: false,
+    });
+}
+
+function getDelay() {
+    if (!(delay.value)) {
+        return DEFAULT_DELAY;
+    }
+    else return delay.value;
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function setMessage(str) {
     message.textContent = str;
     message.hidden = false;
 }
-  
+
 function clearMessage() {
-    message.hidden = true;
     message.textContent = "";
+    message.hidden = true;
 }
